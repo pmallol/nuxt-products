587cab4b7f46416e1141ac785d8dcde10307b15b
pmallol <paulamallol@gmail.com>
2024-06-05 18:46:22 -0300

    feat: Add ProductCard component and update product pages
    The code changes add a new component called ProductCard.vue, which represents a card displaying product information. The component is used in the products index page to render a grid of product cards. Additionally, the product details page and products index page are updated to display the product title, price, and ID.

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <p>Product details for {{id}}</p>
+    <p>{{product.title}}</p>
+    <p>{{product.price}}</p>
+    <p>{{product.id}}</p>
   </div>
 </template>
 
 <script setup>
 const {id} = useRoute().params
+const uri = 'https://fakestoreapi.com/products/' + id
+
+const { data: product } = await useFetch(uri, {key: id})
 
 definePageMeta({
   layout: 'products',
