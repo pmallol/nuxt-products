82c60512304359ec48218c004f54c3032cfd61ac
pmallol <paulamallol@gmail.com>
2024-06-05 19:08:57 -0300

    feat: Handle error and redirect to home page when clicking "Go Home" button
    The code changes in `error.vue` add a button with the text "Go Home" and a click event handler called `handleError`. When the button is clicked, the `handleError` function is called, which clears the error and redirects the user to the home page. This improves the user experience by providing a way to easily navigate back to the home page when encountering an error.

--- error.vue
+++ error.vue
@@ -3,11 +3,17 @@
     <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Oops...</p>
     <p class="mt-7">{{ error.message }}</p>
+
+    <button class="btn my-7" @click="handleError">Go Home</button>
   </div>
 </template>
 
 <script setup>
 defineProps(['error']);
+
+const handleError = () => {
+  clearError({redirect: '/'});
+};
 </script>
 
 <style scoped></style>
